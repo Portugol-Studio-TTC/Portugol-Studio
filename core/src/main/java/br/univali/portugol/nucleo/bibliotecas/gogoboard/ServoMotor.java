a61524ed61cc4858b84ed126b7d9fca76eb3ca08
Ailton Cardoso Júnior <ailtoncardoso0@gmail.com>
2017-09-15 00:13:13 -0300

    Adicionado classes da biblioteca GoGoBoard e suas dependencias no Gradle

---
+++ core/src/main/java/br/univali/portugol/nucleo/bibliotecas/gogoboard/ServoMotor.java
@@ -0,0 +1,14 @@
+package br.univali.portugol.nucleo.bibliotecas.gogoboard;
+
+/**
+ *
+ * @author Ailton Cardoso Jr
+ */
+public class ServoMotor extends Motor
+{
+    public ServoMotor(int numMotor)
+    {
+        super(numMotor);
+    }
+
+}
